7eedb59b32b3452942ef39d8b3a1b8a756359ca6
Achintya Gupta <achintyagupta03@gmail.com>
2022-10-01 16:26:32 +0530

    testing ocr

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 import {loadGLTF} from "./libs/loader.js";
 const THREE = window.MINDAR.IMAGE.THREE;
-let recognized = document.querySelector("#recognized");
-let container = document.querySelector("#container");
 
 document.addEventListener('DOMContentLoaded', () => {
   const start = async() => {
+    // let frame = captureVideoFrame("video", "png");
+    // frame = frame.dataUri;
+    // let ocrResult = writtenOCR(frame);
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
-      imageTargetSrc: './assets/targets/musicband.mind',
+      imageTargetSrc: './assets/targets/kanjimarker.mind',
       maxTrack: 5,
     });
     const {renderer, scene, camera} = mindarThree;
@@ -15,24 +16,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
     scene.add(light);
 
-    const raccoon = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
-    raccoon.scene.scale.set(0.1, 0.1, 0.1);
-    raccoon.scene.position.set(0, -0.4, 0);
-
-    const bear = await loadGLTF('./assets/models/musicband-bear/scene.gltf');
-    bear.scene.scale.set(0.1, 0.1, 0.1);
-    bear.scene.position.set(0, -0.4, 0);
-
-    const raccoonAnchor = mindarThree.addAnchor(0);
-    raccoonAnchor.group.add(raccoon.scene);
-
-    const bearAnchor = mindarThree.addAnchor(1);
-    bearAnchor.group.add(bear.scene);
+    let ocrResult = "Tajmahal";
+    if(ocrResult == "Tajmahal"){
+      const raccoon = await loadGLTF('./assets/models/musicband-raccoon/scene.gltf');
+      raccoon.scene.scale.set(0.1, 0.1, 0.1);
+      raccoon.scene.position.set(0, -0.4, 0);
+      const raccoonAnchor = mindarThree.addAnchor(0);
+      raccoonAnchor.group.add(raccoon.scene);
+    }
+    else{
+      const bear = await loadGLTF('./assets/models/musicband-bear/scene.gltf');
+      bear.scene.scale.set(0.1, 0.1, 0.1);
+      bear.scene.position.set(0, -0.4, 0);
+      const bearAnchor = mindarThree.addAnchor(1);
+      bearAnchor.group.add(bear.scene);
+    }
 
     await mindarThree.start();
-    let frame = captureVideoFrame("video", "png");
-    frame = frame.dataUri;
-    writtenOCR(frame);
     renderer.setAnimationLoop(() => {
       renderer.render(scene, camera);
     });
@@ -99,8 +99,10 @@ function writtenOCR(frame) {
           //         recognized.setAttribute("style", "visibility: hidden");
           //     }, 2000);
           // } else {
-              let result = json_response.data[1].data[0][0];
-              console.log("OCR is: " + result);
+
+          let result = json_response.data[1].data[0][0];
+          console.log("OCR is: " + result);
+          return result;
 
           //     switch (result) {
           //         case "TAJMAHAL":
