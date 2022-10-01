51831f4616252cab92688cbb4d1f54fcff244881
Achintya Gupta <achintyagupta03@gmail.com>
2022-10-01 22:33:01 +0530

    world tracking

--- main.js
+++ main.js
@@ -1,46 +1,82 @@
-import {loadGLTF} from "./libs/loader.js";
-
-const THREE = window.MINDAR.IMAGE.THREE;
+import * as THREE from './libs/three.js-r132/build/three.module.js';
+import {GLTFLoader} from './libs/three.js-r132/examples/jsm/loaders/GLTFLoader.js'
+import {ARButton} from './libs/three.js-r132/examples/jsm/webxr/ARButton.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const start = async() => {
-    let frame = captureVideoFrame("video", "png");
-    frame = frame.dataUri;
-    let ocrResult = writtenOCR(frame);
-    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-      container: document.body,
-      imageTargetSrc: './assets/targets/kanjimarker.mind',
-      maxTrack: 5,
-    });
-    const {renderer, scene, camera} = mindarThree;
+  const initialize = async () => {
+    // let frame = captureVideoFrame("video", "png");
+    // frame = frame.dataUri;
+    // let ocrResult = writtenOCR(frame);
 
-    const light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+    // create AR object
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera();
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
     scene.add(light);
 
-    switch(ocrResult){
-      case "Fox" : 
-      const raccoon = await loadGLTF('./assets/models/Fox.gltf');
-      raccoon.scene.scale.set(0.01, 0.01, 0.01);
-      raccoon.scene.position.set(0, -0.4, 0);
-      const raccoonAnchor = mindarThree.addAnchor(0);
-      raccoonAnchor.group.add(raccoon.scene)
-      break;
+    const reticleGeometry = new THREE.RingGeometry(0.15, 0.2, 32).rotateX(- Math.PI / 2);
+    const reticleMaterial = new THREE.MeshBasicMaterial();
+    const reticle = new THREE.Mesh(reticleGeometry, reticleMaterial);
+    reticle.matrixAutoUpdate = false;
+    reticle.visible = false;
+    scene.add(reticle);
 
-      case "Duck" : 
-      const bear = await loadGLTF('./assets/models/Duck.gltf');
-      bear.scene.scale.set(0.7, 0.7, 0.7);
-      bear.scene.position.set(0, -0.4, 0);
-      const bearAnchor = mindarThree.addAnchor(0);
-      bearAnchor.group.add(bear.scene);
-      break;
-    }
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.xr.enabled = true;
 
-    await mindarThree.start();
-    renderer.setAnimationLoop(() => {
-      renderer.render(scene, camera);
+    const arButton = ARButton.createButton(renderer, { requiredFeatures: ['hit-test'], optionalFeatures: ['dom-overlay'], domOverlay: { root: document.body } });
+    document.body.appendChild(renderer.domElement);
+    document.body.appendChild(arButton);
+
+    const controller = renderer.xr.getController(0);
+    scene.add(controller);
+    const loader = new GLTFLoader();
+    controller.addEventListener('select', () => {
+      loader.load('./assets/models/Duck.gltf', function(gltf) {
+        let duck = gltf.scene.children[0];
+        duck.scale.set(0.01, 0.01, 0.01);
+        duck.position.set(0, 0, -5);
+        duck.position.applyMatrix4(controller.matrixWorld);
+        duck.quaternion.setFromRotationMatrix(controller.matrixWorld);
+        scene.add(gltf.scene);
+      })
     });
+
+    renderer.xr.addEventListener("sessionstart", async (e) => {
+      const session = renderer.xr.getSession();
+      const viewerReferenceSpace = await session.requestReferenceSpace("viewer");
+      const hitTestSource = await session.requestHitTestSource({ space: viewerReferenceSpace });
+
+      renderer.setAnimationLoop((timestamp, frame) => {
+        if (!frame) return;
+
+        const hitTestResults = frame.getHitTestResults(hitTestSource);
+
+        if (hitTestResults.length) {
+          const hit = hitTestResults[0];
+          const referenceSpace = renderer.xr.getReferenceSpace(); // ARButton requested 'local' reference space
+          const hitPose = hit.getPose(referenceSpace);
+
+          reticle.visible = true;
+          reticle.matrix.fromArray(hitPose.transform.matrix);
+        } else {
+          reticle.visible = false;
+        }
+
+        renderer.render(scene, camera);
+      });
+    });
+
+    renderer.xr.addEventListener("sessionend", () => {
+      console.log("session end");
+    });
+
   }
-  start();
+  initialize();
 });
 
 // Capture video frame
@@ -64,65 +100,66 @@ function captureVideoFrame(video, format) {
   let buf = new ArrayBuffer(bytes.length);
   let arr = new Uint8Array(buf);
   for (let i = 0; i < bytes.length; i++) {
-      arr[i] = bytes.charCodeAt(i);
+    arr[i] = bytes.charCodeAt(i);
   }
   let blob = new Blob([arr], {
-      type: mimeType
+    type: mimeType
   });
   return {
-      blob: blob,
-      dataUri: dataUri,
-      format: format,
-      width: canvas.width,
-      height: canvas.height
+    blob: blob,
+    dataUri: dataUri,
+    format: format,
+    width: canvas.width,
+    height: canvas.height
   };
 };
 
 // OCR API
 function writtenOCR(frame) {
   fetch('https://hf.space/embed/tomofi/MaskTextSpotterV3-OCR/+/api/predict/', {
-  // fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUoNOlhCK2chd2UJGOZA4uYEHxztzuh4M', {
-      method: "POST",
-      body: JSON.stringify({
-        "data": ["data:" + frame]
+    // fetch('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAUoNOlhCK2chd2UJGOZA4uYEHxztzuh4M', {
+    method: "POST",
+    body: JSON.stringify({
+      "data": ["data:" + frame]
     }),
-      headers: {
-          "Content-Type": "application/json"
-      }
+    headers: {
+      "Content-Type": "application/json"
+    }
   }).then(
-      function (response) {
-          return response.json();
-      })
-      .then(function (json_response) {
-          let arrayLength = json_response.data[1].data;
-          
-          console.log("arrayLength is: " + arrayLength);
-          if (arrayLength == 0) {
-            window.location.reload();
-              // recognized.innerHTML = "Please scan one more time...";
-          //     setTimeout(() => {
-          //         container.setAttribute("style", "visibility: hidden");
-          //         recognized.setAttribute("style", "visibility: hidden");
-          //     }, 2000);
-          // } else {
-              let result = json_response.data[1].data[0][0];
-              console.log("OCR is: " + result);
-              return result;
+    function (response) {
+      return response.json();
+    })
+    .then(function (json_response) {
+      let arrayLength = json_response.data[1].data;
 
-          //     switch (result) {
-          //         case "TAJMAHAL":
-          //         case "Tajmahal":
-          //         case "tajmahal":
-          //             recognized.innerHTML = "Monument: " + result;
-          //             break;
-          //         default:
-          //             recognized.innerHTML = "I see: " + result;
-          //         // code block
-          //     }
-          //     setTimeout(() => {
-          //         container.setAttribute("style", "visibility: hidden");
-          //         recognized.setAttribute("style", "visibility: hidden");
-          //     }, 2000);
-          // }
-      }})
+      console.log("arrayLength is: " + arrayLength);
+      if (arrayLength == 0) {
+        window.location.reload();
+        // recognized.innerHTML = "Please scan one more time...";
+        //     setTimeout(() => {
+        //         container.setAttribute("style", "visibility: hidden");
+        //         recognized.setAttribute("style", "visibility: hidden");
+        //     }, 2000);
+        // } else {
+        let result = json_response.data[1].data[0][0];
+        console.log("OCR is: " + result);
+        return result;
+
+        //     switch (result) {
+        //         case "TAJMAHAL":
+        //         case "Tajmahal":
+        //         case "tajmahal":
+        //             recognized.innerHTML = "Monument: " + result;
+        //             break;
+        //         default:
+        //             recognized.innerHTML = "I see: " + result;
+        //         // code block
+        //     }
+        //     setTimeout(() => {
+        //         container.setAttribute("style", "visibility: hidden");
+        //         recognized.setAttribute("style", "visibility: hidden");
+        //     }, 2000);
+        // }
+      }
+    })
 }
